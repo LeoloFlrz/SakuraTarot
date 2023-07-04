397adf2b837985e8eb9e992eaf4829cb2907feff
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-07-04 14:22:14 +0200

    :(
    rebase from dev

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
+	import CardStages from './components/CardStages.vue';
 	import {ref} from "vue"
 	import CardsSakura from './components/CardsSakura.vue'
 	const objectFromChild = ref();
+
+
 </script>
 
 <template>
 	<main>
 		<CardsSakura @response="(data) => objectFromChild = data"/>
 	</main>
+	<CardStages />
 </template>
 
 <style></style>
+	
+
---
+++ src/components/CardStages.vue
@@ -0,0 +1,106 @@
+
+<script setup>
+import { ref } from 'vue';
+
+    defineProps({
+        contador: {
+            type: String
+        }
+    })
+
+    
+
+   
+    
+    // onUpdated(() => {
+    //     try {
+    //         if(contadorEtapas.value === 0) {
+    //             etapa.value = "Pasado"
+    //         } else if (contadorEtapas.value === 1) {
+    //             etapa.value = "Presente"
+    //         } else if (contadorEtapas.value === 2) {
+    //             etapa.value = "Futuro"
+    //         } else {
+    //             contadorEtapas.value = 0
+    //             etapa.value = "Default"
+    //         } 
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    
+    // })
+
+    // try {
+
+    //     let etapa = "";
+    //     const contadorEtapas = ref();
+
+    //         if(contadorEtapas.value == 0) {
+    //             etapa = "Pasado"
+    //         } else if (contadorEtapas.value == 1) {
+    //             etapa = "Presente"
+    //         } else if (contadorEtapas.value == 2) {
+    //             etapa = "Futuro"
+    //         } else {
+    //             contadorEtapas.value = 0
+    //             etapa = "Default"
+    //         } 
+
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+
+
+    const cambiarEtapa = () => {
+        const etapa = ref()
+        const contadorEtapas = ref()
+
+        try {
+            if (contadorEtapas.value == 0) {
+                etapa.value = "pasado"
+            } else if (contadorEtapas.value == 1) {
+                etapa.value = "presente"
+            } else if (contadorEtapas.value == 2) {
+                etapa.value = "futuro"
+            } else {
+                etapa.value = "Default"
+                contadorEtapas.value = 0
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        return etapa.value
+    }
+    // let contadorPrueba = computed(() =>{
+            
+    //         return contadorPrueba.value
+    //     })
+    // onMounted(() => {
+    //     try {
+    //         if(contadorEtapas.value === 0) {
+    //             etapa.value = "Pasado"
+    //         } else if (contadorEtapas.value === 1) {
+    //             etapa.value = "Presente"
+    //         } else if (contadorEtapas.value === 2) {
+    //             etapa.value = "Futuro"
+    //         } else {
+    //             contadorEtapas.value = 0
+    //             etapa.value = "Default"
+    //         } 
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // })
+
+</script>
+
+<template>
+    <h2>Conoce tu {{ etapa }}</h2>
+    <v-btn @click="cambiarEtapa, contadorEtapas++">
+        {{ contadorEtapas }}
+    </v-btn>
+</template>
+
+<style>
+</style>
+

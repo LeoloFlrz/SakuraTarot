4bb9ea523f5846ac5cab536c2e842aef5b214890
LuLu <lu.luzan@gmail.com>
2023-07-05 09:59:48 +0200

    ;

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
-	import {ref} from "vue"
-	import CardsSakura from './components/CardsSakura.vue'  import BtnReset from './components/BtnReset.vue';
+	import {ref} from "vue";
+	import CardsSakura from './components/CardsSakura.vue';
+	import BtnReset from './components/BtnReset.vue';
   
 	const objectFromChild = ref();
 </script>
 
 <template>
 <main>
-  <BtnReset />
-		<CardsSakura @response="(data) => objectFromChild = data"/>
+	<BtnReset />
+	<CardsSakura @response="(data) => objectFromChild = data"/>
 	</main>
 </template>
 

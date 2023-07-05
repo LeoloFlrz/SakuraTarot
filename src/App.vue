1e0e7110d27245949a2ba49ac32be228885655df
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-07-05 17:22:44 +0200

    readded cardStages to app

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import CardsSakura from './components/CardsSakura.vue';
 //import CardsDeck from './components/CardsDeck.vue';
 import BtnReset from './components/BtnReset.vue';
+import CardStages from "./components/CardStages.vue";
 const BooleanShuffle = ref();
 const objectFromChild = ref();
 </script>
@@ -12,6 +13,7 @@ const objectFromChild = ref();
 <main>
 	<BtnReset @response="(reparto) => BooleanShuffle = reparto"/>
 	<CardsSakura v-if = "BooleanShuffle" @response="(data) => objectFromChild = data"/>
+	<CardStages />
 </main>
 </template>
 
--- src/components/CardStages.vue
+++ src/components/CardStages.vue
@@ -26,43 +26,43 @@ import count from './CardsSakura.vue'
     
     const etapa = ref('');
     const contadorEtapas = ref(0);
-    const cartaSeleccionada = ref('');
-    // const pasadoFlag = ref(false)
-    // const presenteFlag = ref(false)
-    // const futuroFlag = ref(false)
+    // const cartaSeleccionada = ref('');
+    const pasadoFlag = ref(false)
+    const presenteFlag = ref(false)
+    const futuroFlag = ref(false)
 
-//     const cambiarEtapa = () => {
-//     if (contadorEtapas.value === 0) {
-//       etapa.value = 'pasado';
-//       pasadoFlag.value = true;
-//     } else if (contadorEtapas.value === 1) {
-//       etapa.value = 'presente';
-//       pasadoFlag.value = false;
-//       presenteFlag.value = true;
-//     } else if (contadorEtapas.value === 2) {
-//       etapa.value = 'futuro';
-//       presenteFlag.value = false;
-//       futuroFlag.value = true;
-//     } else {
-//       etapa.value = 'pasado';
-//       contadorEtapas.value = 0;
-//       presenteFlag.value = false;
-//       futuroFlag.value = false;
-//       pasadoFlag.value = true;
-//     }
-//   };
+    const cambiarEtapa = () => {
+    if (contadorEtapas.value === 0) {
+      etapa.value = 'pasado';
+      pasadoFlag.value = true;
+    } else if (contadorEtapas.value === 1) {
+      etapa.value = 'presente';
+      pasadoFlag.value = false;
+      presenteFlag.value = true;
+    } else if (contadorEtapas.value === 2) {
+      etapa.value = 'futuro';
+      presenteFlag.value = false;
+      futuroFlag.value = true;
+    } else {
+      etapa.value = 'pasado';
+      contadorEtapas.value = 0;
+      presenteFlag.value = false;
+      futuroFlag.value = false;
+      pasadoFlag.value = true;
+    }
+  };
 
-    const cambiarEtapa = (carta) => {
-        if (contadorEtapas.value === 0) {
-        etapa.value = 'pasado';
-        } else if (contadorEtapas.value === 1) {
-        etapa.value = 'presente';
-        } else if (contadorEtapas.value === 2) {
-        etapa.value = 'futuro';
-        }
-        contadorEtapas.value = (contadorEtapas.value + 1) % 3;
-        cartaSeleccionada.value = carta;
-    };
+    // const cambiarEtapa = (carta) => {
+    //     if (contadorEtapas.value === 0) {
+    //     etapa.value = 'pasado';
+    //     } else if (contadorEtapas.value === 1) {
+    //     etapa.value = 'presente';
+    //     } else if (contadorEtapas.value === 2) {
+    //     etapa.value = 'futuro';
+    //     }
+    //     contadorEtapas.value = (contadorEtapas.value + 1) % 3;
+    //     cartaSeleccionada.value = carta;
+    // };
   try {
     if (cards[0] != '' && cards[1] != '') {
     console.log(cards[0]);

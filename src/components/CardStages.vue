eccfe6371a95e9836fefa59647ba5770360ef821
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-07-05 09:31:43 +0200

    little changes

--- src/components/CardStages.vue
+++ src/components/CardStages.vue
@@ -11,17 +11,28 @@ import { ref } from 'vue';
     
     const etapa = ref('');
     const contadorEtapas = ref(0);
+    const pasadoFlag = ref(false)
+    const presenteFlag = ref(false)
+    const futuroFlag = ref(false)
 
     const cambiarEtapa = () => {
     if (contadorEtapas.value === 0) {
       etapa.value = 'pasado';
+      pasadoFlag.value = true;
     } else if (contadorEtapas.value === 1) {
       etapa.value = 'presente';
+      pasadoFlag.value = false;
+      presenteFlag.value = true;
     } else if (contadorEtapas.value === 2) {
       etapa.value = 'futuro';
+      presenteFlag.value = false;
+      futuroFlag.value = true;
     } else {
       etapa.value = 'pasado';
       contadorEtapas.value = 0;
+      presenteFlag.value = false;
+      futuroFlag.value = false;
+      pasadoFlag.value = true;
     }
   };
     

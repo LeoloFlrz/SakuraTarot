27e5c83fad8ef287a4c97ce26f2d83402d0c9eab
César Conte Pérez <128363248+cesarconte@users.noreply.github.com>
2023-07-05 12:59:03 +0200

    done cardsakura2.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 	import {ref} from "vue"
-	import CardsSakura from './components/CardsSakura.vue'
+	import CardsSakura2 from './components/CardsSakura2.vue'
 	const objectFromChild = ref();
 </script>
 
 <template>
 	<main>
-		<CardsSakura @response="(data) => objectFromChild = data"/>
+		<CardsSakura2 @response="(data) => objectFromChild = data"/>
 	</main>
 </template>
 

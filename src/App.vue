6bb2f69077fa9130b4edfb67051d8a8a32b2d5c5
LuLu <lu.luzan@gmail.com>
2023-07-05 14:51:21 +0200

    functional

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 	import {ref} from "vue"
-	import CardsSakura from './components/CardsSakura.vue'  import BtnReset from './components/BtnReset.vue';
-  
+	import CardsSakura from './components/CardsSakura.vue'
+	import BtnReset from './components/BtnReset.vue';
+	const BooleanShuffle = ref();
 	const objectFromChild = ref();
 </script>
 
 <template>
 <main>
-  <BtnReset />
-		<CardsSakura @response="(data) => objectFromChild = data"/>
+	<BtnReset @response="(reparto) => BooleanShuffle = reparto"/>
+	<CardsSakura v-if = "BooleanShuffle" @response="(data) => objectFromChild = data"/>
 	</main>
 </template>
 

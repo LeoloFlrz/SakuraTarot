73a5ed1079ec0f2f1f3924873631640224e314f1
LuLu <lu.luzan@gmail.com>
2023-07-06 12:38:29 +0200

    break_footer

--- src/components/FooterSakura.vue
+++ src/components/FooterSakura.vue
@@ -1,5 +1,44 @@
-<template>
+<!-- <template>
+    <footer class="footer">
+        <div>
+            <img src="imgs/Site-logo_sakura" />
+        </div>
+        <p>&copy; {{ currentYear }} Sakura CardCaptor</p>
+    </footer>
+  </template>
 
-</template>
 <style>
+
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script> -->
+<template>
+    <footer class="footer">
+        <div>
+            <img src="imgs/Site-logo_sakura" />
+        </div>
+        <p>&copy; {{ currentYear }} Sakura CardCaptor</p>
+    </footer>
+  </template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>

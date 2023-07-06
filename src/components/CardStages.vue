4ff0ccb84144be597fd3675e74feb9d7f9444935
LeoloFlrz <leoloflrzr@gmail.com>
2023-07-06 19:04:32 +0200

    added img and meaning

--- src/components/CardStages.vue
+++ src/components/CardStages.vue
@@ -13,9 +13,9 @@ const props = defineProps({
   }
 });
 
-let card1;
-let card2;
-let card3;
+let card1, card1Img, card1Meaning;
+let card2, card2Img, card2Meaning;
+let card3, card3Img, card3Meaning;
 // const contador = ref(0);
 const arraySeleccionadas = []
 
@@ -46,18 +46,24 @@ onBeforeUpdate(() => {
 
     if (arraySeleccionadas.length === 1) {
       card1 = arraySeleccionadas[0]
+      card1Img = arraySeleccionadas[0].sakuraCard
+      card1Meaning = arraySeleccionadas[0].meaning
       console.log(card1);
     }
      if (arraySeleccionadas.length === 2 ) {
       card2 = arraySeleccionadas[1]
+      card2Img = arraySeleccionadas[1].sakuraCard
+      card2Meaning = arraySeleccionadas[1].meaning
       console.log(card2);
     } 
      if (arraySeleccionadas.length === 3 ) {
       card3 = arraySeleccionadas[2]
+      card3Img = arraySeleccionadas[2].sakuraCard
+      card3Meaning = arraySeleccionadas[2].meaning
       console.log(card3);
     }
     console.log(arraySeleccionadas);
-    return card1, card2, card3
+    return { card1, card1Img, card1Meaning, card2, card2Img, card2Meaning, card3, card3Img, card3Meaning}
 })
 
 
@@ -67,9 +73,21 @@ onBeforeUpdate(() => {
   <div class="containerEtapa">
     <h2>Conoce tu destino</h2>
     <div class="containerSeleccionadas">
-      <div class="containerPasado">Pasado {{ card1 }}</div>
-      <div class="containerPresente">Presente {{ card2 }}</div>
-      <div class="containerFuturo">Futuro {{ card3 }}</div>
+      <div class="containerPasado">
+        <p>Pasado {{ card1 }}</p> 
+        <img  >{{ card1Img }}<img/>
+        <p>{{ card1Meaning }}</p>
+      </div>
+      <div class="containerPresente">
+        <p>Presente {{ card2 }}</p> 
+        <img>{{ card2Img }}<img/>
+        <p>{{ card2Meaning }}</p>
+      </div>
+      <div class="containerFuturo">
+        <p>Futuro {{ card3 }}</p>
+        <img> {{ card3Img }} <img/>
+        <p>{{ card3Meaning }}</p>
+      </div>
     </div>
   </div>
 </template>

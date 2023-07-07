441e120d8b32499eb6955522eb51659db5258a4c
LuLu <lu.luzan@gmail.com>
2023-07-07 11:37:29 +0200

    magicWandImg

--- src/components/FooterSakura.vue
+++ src/components/FooterSakura.vue
@@ -1,7 +1,8 @@
 
 <template>
     <footer>  
-        <div class="bg-footer">Sakura CardCaptor  {{ currentYear }}
+        <div class="bg-footer">
+            <p>&copy; Sakura CardCaptor  {{ currentYear }}</p>
         <!-- <img src="imgs/Site-logo_sakura.webp" /> -->
         </div>
     </footer>
@@ -12,6 +13,7 @@
 footer{
     position: sticky;
     background-image:url(imgs/bg_sakuras.png);
+    width: 100%;
 }
 
 </style>

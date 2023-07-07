2b08730ad09a6857ca483e91ae09e5ea5a239ba0
César Conte Pérez <128363248+cesarconte@users.noreply.github.com>
2023-07-07 14:45:15 +0200

    .containerCard with done

--- src/components/CardsSakura.vue
+++ src/components/CardsSakura.vue
@@ -51,29 +51,27 @@
 </script>
 
 <template>
-		<v-container class="align-center justify-center containerCard">
+		<v-container class="align-center justify-center containerCard my-6">
 			<v-row>
-				<v-col cols="4" sm="4" md="4" lg="2" v-for="n in 12" :key="n">
-					<v-img src="/imgs/sakuraReverse.jpg" @click="showCard(n-1)" elevation="12" class="imageCard mx-auto rounded-lg pa-2"></v-img>
+				<v-col cols="4" sm="4" md="2" lg="2" v-for="n in 12" :key="n">
+					<v-img src="/imgs/sakuraReverse.jpg" @click="showCard(n-1)" class="imageCard mx-auto rounded-lg pa-2"></v-img>
 				</v-col>
 			</v-row>
 		</v-container>
-		<v-overlay v-model="overlay" class="align-center justify-center" scrim="#795176" @click:outside="overlay = false" >
+		<v-overlay v-model="overlay" class="align-center justify-center" scrim="#795176" @click:outside="overlay = false">
 			<v-img :src="clickCard" class="rounded-lg"></v-img>
 		</v-overlay>
 </template>
 
 
 <style scoped>
-/*scrim="#f9a245"*/
 .containerCard{
-    max-width: 50vmax;
+    width: 65vmax;
 }
 
-@media screen and (max-width: 420px) {
+@media screen and (max-width: 960px) {
 .containerCard {
-    max-width: 30vmax;
-    margin: 20px auto;
+    width: 33vmax;
     padding: 0;
   }
 }
